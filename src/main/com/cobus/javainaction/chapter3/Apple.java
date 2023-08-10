c295a177f439d701a63c4e45e41d1dbefe2e3775
cestlavie01 <falloutvoy@gmail.com>
2023-08-10 19:01:41 +0900

    chapter 3

---
+++ src/main/com/cobus/javainaction/chapter3/Apple.java
@@ -0,0 +1,7 @@
+package com.cobus.javainaction.chapter3;
+
+public class Apple extends Fruit {
+	public Apple(Integer weight) {
+		super(weight);
+	}
+}
---
+++ src/main/com/cobus/javainaction/chapter3/Fruit.java
@@ -0,0 +1,29 @@
+package com.cobus.javainaction.chapter3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Fruit {
+	static Map<String, Function<Integer, Fruit>> map = new HashMap<>();
+	static {
+		map.put("apple", Apple::new);
+		map.put("orange", Orange::new);
+		// add more
+	}
+	
+	public static Fruit giveMeFruit(String fruit, Integer weight) {
+		return map.get(fruit.toLowerCase()).apply(weight);
+	}
+	
+	private Integer weight = 0;
+
+	public Fruit(Integer weight) {
+		this.weight = weight;
+	}
+}
