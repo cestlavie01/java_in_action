b64b9b06481b93e702a1de467291adf774db8ab5
cestlavie01 <falloutvoy@gmail.com>
2023-08-30 23:34:40 +0900

    1장 들여쓰기 수정

--- src/main/java/com/cobus/javainaction/chapter_1/Chapter1Main.java
+++ src/main/java/com/cobus/javainaction/chapter_1/Chapter1Main.java
@@ -15,30 +15,29 @@ public class Chapter1Main {
 		inventory.add(new Apple(Apple.Color.RED, 60));
 		inventory.add(new Apple(Apple.Color.RED, 160));
 		inventory.add(new Apple(Apple.Color.BLUE, 260));
-		
+
 		System.out.println(Apple.filterApples(inventory, Apple::isGreenApple));
 		System.out.println(Apple.filterApples(inventory, Apple::isHeavyApple));
 		System.out.println(Apple.filterApples(inventory, (Apple a) -> Apple.Color.BLUE.equals(a.getColor())));
-		
-		List<Transaction> transactions= new ArrayList<>();
-		
-Map<Currency, List<Transaction>> transactionsByCurrencies = new HashMap<>();
-for(Transaction transaction : transactions) {
-	if (transaction.getPrice() > 1000) {
-		Currency currency = transaction.getCurrency();
-		List<Transaction> transactionForCurrency = transactionsByCurrencies.get(currency);
-		if (transactionForCurrency == null) {
-			transactionForCurrency = new ArrayList<>();
-			transactionsByCurrencies.put(currency, transactionForCurrency);
+
+		List<Transaction> transactions = new ArrayList<>();
+
+		Map<Currency, List<Transaction>> transactionsByCurrencies = new HashMap<>();
+		for (Transaction transaction : transactions) {
+			if (transaction.getPrice() > 1000) {
+				Currency currency = transaction.getCurrency();
+				List<Transaction> transactionForCurrency = transactionsByCurrencies.get(currency);
+				if (transactionForCurrency == null) {
+					transactionForCurrency = new ArrayList<>();
+					transactionsByCurrencies.put(currency, transactionForCurrency);
+				}
+
+				transactionForCurrency.add(transaction);
+			}
 		}
 
-		transactionForCurrency.add(transaction);
+		Map<Currency, List<Transaction>> transactionsByCurrencies2 = transactions.stream()
+				.filter((Transaction t) -> t.getPrice() > 1000)
+				.collect(groupingBy(Transaction::getCurrency));
 	}
-}
-
-Map<Currency, List<Transaction>> transactionsByCurrencies2 = 
-		transactions.stream()
-			.filter((Transaction t) -> t.getPrice() > 1000)
-			.collect(groupingBy(Transaction::getCurrency));
-	}	
 }
